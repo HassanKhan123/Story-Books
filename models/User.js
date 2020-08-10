ad77facc795bd2c76cfa98879223b031b7b59dbb
HassanKhan123 <hk2602809@gmail.com>
2020-08-10 22:12:40 +0500

    save google profile data to database

--- models/User.js
+++ models/User.js
@@ -5,7 +5,6 @@ const UserSchema = new mongoose.Schema({
   displayName: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  googleId: { type: String, required: true },
   image: { type: String },
   createdAt: { type: Date, default: Date.now() },
 });
